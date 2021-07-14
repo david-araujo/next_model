e734e6843cc7063842cf4a75be24a0a5ea9386a4
David Araujo <david.araujo91@gmail.com>
2021-07-14 12:41:43 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Filter Game</title>
+      </Head>
+
+      <main>
+        <h1>Hello Next!!!</h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
+
+export default Home
